4c23cb16dc557afb5fc198622b44d2b3c4bf62ca
Daniel Malmgren de Oliveira <daniel.malmgren_oliveira@ellos.se>
2022-07-06 17:19:16 +0200

    Adding leaflet CDN. Setting up initial test map

---
+++ leaflet.js
@@ -0,0 +1,23 @@
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+const marker = L.marker([51.5, -0.09]).addTo(map);
+
+function success(pos) {
+    const crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error);
