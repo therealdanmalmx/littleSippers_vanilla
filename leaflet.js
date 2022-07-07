1680cd6689fb53484851ab9a2241500c0649df65
therealdanmalmx <danmalmx@gmail.com>
2022-07-07 15:44:05 +0200

    Adding markers from a placeholder array

--- leaflet.js
+++ leaflet.js
@@ -6,6 +6,51 @@ const attribution =
 
 const initialZoom = 15;
 
+const map = L.map("map").setView([0, 0], initialZoom);
+let marker = L.marker([0, 0]).addTo(map);
+
+const locations = [
+  {
+    id: 1,
+    name: "Espresso House Knalleland",
+    address: {
+      street: "Lundbygatan 1",
+      city: "Borås",
+      postcode: "506 38",
+    },
+    coordinates: {
+      latitude: 57.73296,
+      longitude: 12.93726,
+    },
+  },
+  {
+    id: 2,
+    name: "Espresso House Borås Station",
+    address: {
+      street: "Stationsgatan 16",
+      city: "Borås",
+      postcode: "503 38",
+    },
+    coordinates: {
+      latitude: 57.72057,
+      longitude: 12.93258,
+    },
+  },
+  {
+    id: 3,
+    name: "Café Viskan",
+    address: {
+      street: "Södra Strandgatan 6",
+      city: "Borås",
+      postcode: " 503 35",
+    },
+    coordinates: {
+      latitude: 57.71984,
+      longitude: 12.94025,
+    },
+  },
+];
+
 function getLocation() {
   navigator.geolocation.getCurrentPosition(success, error);
 }
@@ -15,7 +60,7 @@ getLocation();
 function success(pos) {
   const crd = pos.coords;
 
-  marker.setLatLng([crd.latitude, crd.longitude]);
+  // marker.setLatLng([crd.latitude, crd.longitude]);
   map.setView([crd.latitude, crd.longitude], initialZoom);
 }
 
@@ -24,11 +69,13 @@ function error(err) {
 }
 
 function getCaffees() {
-  // market.setLatLng([])
+  locations.forEach((item) => {
+    L.marker([item.coordinates.latitude, item.coordinates.longitude]).addTo(
+      map
+    );
+  });
 }
-
-const map = L.map("map").setView([0, 0], initialZoom);
-const marker = L.marker([0, 0]).addTo(map);
+getCaffees();
 
 L.tileLayer(tileUrl, {
   attribution,
