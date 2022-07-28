dc925639dd841d71646f690bc863748735b71abb
therealdanmalmx <danmalmx@gmail.com>
2022-07-28 20:50:27 +0100

    Add placdeholding POST request

--- scripts/newLocation.js
+++ scripts/newLocation.js
@@ -10,6 +10,31 @@
 
 const addNewLocation = () => {
     alert('New location added')
+    // const options = {
+    //     method: 'POST',
+    //     body: {
+    //         data: {
+    //             name: "Café Bakgården",
+    //             street:"Skolgatan 3",
+    //             postal_code:"503 33",
+    //             city:"Borås",
+    //             latitude:"57.722600",
+    //             longitude:"12.939000",
+    //             amenities:{
+    //                 duck:"true",
+    //                 changeroom:"true",
+    //                 literature:"false",
+    //                 playground:"true",
+    //                 playoutside:"false"
+    //             }
+    //         }
+    //     }
+    //   };
+
+    //   fetch('http://localhost:1337/api/cafes', options)
+    //     .then(response => response.json())
+    //     .then(response => console.log(response))
+    //     .catch(err => console.error(err));
 
 }
 
